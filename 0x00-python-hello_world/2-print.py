935f8253b556aa9086d50bcd234eed4e6eb7326b
Ezike <afamefunameanwellezike@gmail.com>
2022-11-29 09:52:57 +0100

    fre

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+msg = "\"programming is like building a multilingual puzzle"
+print(msg)
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str * 3 + f"\n")
+print(f"%s \n" % str[:9])
