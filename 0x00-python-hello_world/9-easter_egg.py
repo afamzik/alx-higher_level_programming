e52745c571fc747829a93dbea1d2dc26aa7bb2ca
Ezike <afamefunameanwellezike@gmail.com>
2022-12-01 02:55:16 +0100

    dertyui

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-str = "The Zen of Python, by Tim Peters\n
-Beautiful is better than ugly.\n
-Explicit is better than implicit.\
-Simple is better than complex.\
-Complex is better than complicated.\
-Flat is better than nested.\
-Sparse is better than dense.\
+str = '''The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
@@ -18,6 +19,5 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!"
-
+Namespaces are one honking great idea -- let's do more of those!'''
 print(str)
