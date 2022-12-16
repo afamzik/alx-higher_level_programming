e415aae608e6f1f30c8e0809062ed4022230ced5
Ezike <afamefunameanwellezike@gmail.com>
2022-12-16 19:44:16 +0100

    do

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     idx = 0
     while idx < x:
@@ -6,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[idx]), end="")
         except IndexError:
             break
-        idx += 1
+        idx = idx + 1
     print("")
     return idx
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    printed = 0
+    xy = 0
     for idx in range(x):
         try:
             print("{:d}".format(my_list[idx]), end="")
-            printed += 1
+            xy = xy + 1
         except (TypeError, ValueError):
             continue
     print("")
-    return printed
+
+    return xy
