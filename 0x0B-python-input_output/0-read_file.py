99bbfdcec9ecf6d2c1d8ad7784c1ba4b3f86dd0b
Ezike <afamefunameanwellezike@gmail.com>
2022-12-17 07:04:27 +0100

    fdr

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def read_file(filename=""):
-    """Read and print text from file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read(), end="")
--- 0x0B-python-input_output/1-number_of_lines.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        lines = 0
-        for line in f:
-            lines += 1
-    return lines
