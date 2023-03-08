5ba2abb4310f510be78367d7d7754de88f20ff48
afamzik <afamefunameanwellezike@gmail.com>
2023-03-08 01:09:44 +0100

    if_else

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for y in range(ord('a'), ord('z')+1):
+    print('{}'.format(chr(y)), end='')
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for c in range(ord('a'), ord('z')+1):
+    if y is not (ord('q')) and y is not (ord('e')):
+        print('{}'.format(chr(y)), end='')
